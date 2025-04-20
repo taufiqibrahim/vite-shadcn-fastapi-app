5cc06f5a3739e97e8e8ca2d7121033d2f5b11ce2
Taufiq Ibrahim <taufiq.ibrahim@gmail.com>
2025-04-20 19:12:05 +0700

    frontend: improve lib/maps.ts typesafety

--- frontend/src/lib/maps.ts
+++ frontend/src/lib/maps.ts
@@ -1,13 +1,16 @@
 import { ACCESS_TOKEN_KEY, API_BASE_URL } from "@/constants";
+import maplibregl from "maplibre-gl";
 
-
-export const transformRequest = (url: any, resourceType: any) => {
-    if (resourceType === 'Tile' && url.indexOf(API_BASE_URL) > -1) {
-        const token = localStorage.getItem(ACCESS_TOKEN_KEY);
-        return {
-            url: url,
-            headers: {'Authorization': `Bearer ${token}`},
-            credentials: 'include'
-        };
-    }
-}
+export const transformRequest: maplibregl.RequestTransformFunction = (
+  url: any,
+  resourceType: any,
+) => {
+  if (resourceType === "Tile" && url.indexOf(API_BASE_URL) > -1) {
+    const token = localStorage.getItem(ACCESS_TOKEN_KEY);
+    return {
+      url: url,
+      headers: { Authorization: `Bearer ${token}` },
+      credentials: "include",
+    };
+  }
+};
