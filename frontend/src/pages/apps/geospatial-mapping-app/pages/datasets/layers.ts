95834587b0d6180e6620e544cefa9c6d8f5c3331
Taufiq Ibrahim <taufiq.ibrahim@gmail.com>
2025-05-18 12:25:02 +0700

    remove geospatial-mapping-app

--- frontend/src/pages/apps/geospatial-mapping-app/pages/datasets/layers.ts
+++
@@ -1,5 +0,0 @@
-// import {Map, Source, Layer} from 'react-map-gl/maplibre';
-// import type {CircleLayer} from 'react-map-gl/maplibre';
-// import type {LineLayerSpecification} from 'react-map-gl/maplibre';
-
-// export const datasetMVTLineLayer
--- frontend/src/pages/apps/geospatial-mapping-app/types.ts
+++
@@ -1,6 +0,0 @@
-export interface DatasetMapsProps {
-  datasetUid: string;
-  primaryKeyColumn: string;
-  bbox?: [number, number, number, number];
-  setBBox?: (bbox: [number, number, number, number]) => void;
-}
